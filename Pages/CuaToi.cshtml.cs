7b319141b5a1ceb560786887b460129a04b15d94
phamthecuong2022006-alt <phamthecuong2022006@gmail.com>
2026-09-16 09:48:26 +0700

    push project lesson1

---
+++ Pages/CuaToi.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lesson1.Pages;
+
+public class CuaToi : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lesson1.Pages.Shared;
+
+public class _Layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
